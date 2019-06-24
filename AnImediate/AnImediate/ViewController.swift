c45e25a89ccded54c3635480bd6708d6df4727c7
k-shuya <kwamua@me.com>
2019-06-24 15:29:44 +0900

    プロジェクトの作成

---
+++ AnImediate/AnImediate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnImediate
+//
+//  Created by 川村周也 on 2019/06/24.
+//  Copyright © 2019 AI_Love_DeepLearning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
