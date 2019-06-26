805fe751e959a752a3898238aaa280eb5ba78435
川上達也 <>
2019-06-26 16:41:37 +0900

    SettingVCを追加

---
+++ AnImediate/AnImediate/SettingVC.swift
@@ -0,0 +1,30 @@
+//
+//  SettingVC.swift
+//  AnImediate
+//
+//  Created by 川上達也 on 2019/06/26.
+//  Copyright © 2019 AI_Love_DeepLearning. All rights reserved.
+//
+
+import UIKit
+
+class SettingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
