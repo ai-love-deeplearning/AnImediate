b7c89aff747cf9a632668f2f6472bb2e0b926ca4
川上達也 <>
2019-06-26 17:18:50 +0900

    TableViewを追加

--- AnImediate/AnImediate/SettingVC.swift
+++ AnImediate/AnImediate/SettingVC.swift
@@ -10,21 +10,32 @@ import UIKit
 
 class SettingVC: UIViewController {
 
+    @IBOutlet weak var settingTableView: UITableView!
+    
+    let settingItem: [String] = ["アカウント","使い方","お問い合わせ","プライバシー","ライセンス"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        settingTableView.delegate = self
+        settingTableView.dataSource = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension SettingVC: UITableViewDelegate {
+    
+}
+
+extension SettingVC:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingItem.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = settingTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) 
+        cell.textLabel?.text = settingItem[indexPath.row]
+        // 色仮置き
+        cell.textLabel?.textColor = UIColor.red
+        return cell
     }
-    */
-
 }
