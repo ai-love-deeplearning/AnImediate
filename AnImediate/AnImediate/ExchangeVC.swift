9432e814941140bea099f8e5cc794930c2ebb225
k-shuya <kwamua@me.com>
2019-06-28 09:28:08 +0900

    add files

---
+++ AnImediate/AnImediate/ExchangeVC.swift
@@ -0,0 +1,30 @@
+//
+//  ExchangeVC.swift
+//  AnImediate
+//
+//  Created by 川村周也 on 2019/06/27.
+//  Copyright © 2019 AI_Love_DeepLearning. All rights reserved.
+//
+
+import UIKit
+
+class ExchangeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
