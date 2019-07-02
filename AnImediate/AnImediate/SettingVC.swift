a5b8be42e16d70588823bb4ee30306a2b46471e9
k-shuya <kwamua@me.com>
2019-07-02 13:26:27 +0900

    ログアウト機能の実装

--- AnImediate/AnImediate/SettingVC.swift
+++ AnImediate/AnImediate/SettingVC.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import Firebase
 
 class SettingVC: UIViewController {
 
     @IBOutlet weak var settingTableView: UITableView!
     
-    let settingItem: [String] = ["アカウント","使い方","お問い合わせ","プライバシー","ライセンス","ログアウト","退会"]
+    let settingItem: [String] = ["アカウント", "使い方", "お問い合わせ", "プライバシー", "ライセンス", "ログアウト", "退会"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,17 @@ class SettingVC: UIViewController {
         settingTableView.delegate = self
         settingTableView.dataSource = self
     }
+    
+    private func showErrorIfNeeded(_ errorOrNil: Error?) {
+        // エラーがなければ何もしません
+        guard errorOrNil != nil else { return }
+        
+        let message = "エラーが起きました"
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
 extension SettingVC: UITableViewDelegate {
@@ -47,6 +59,23 @@ extension SettingVC:UITableViewDataSource{
             self.performSegue(withIdentifier: "toUse", sender: self)
         case 2:
             self.performSegue(withIdentifier: "toMail", sender: self)
+        case 3:
+            self.performSegue(withIdentifier: "toMail", sender: self)
+        case 4:
+            self.performSegue(withIdentifier: "toMail", sender: self)
+        case 5:
+            let alert = UIAlertController(title: "ログアウト", message: "ログアウトしますか？", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {
+                (action) in
+                do {
+                    try Auth.auth().signOut()
+                    self.performSegue(withIdentifier: "toLogin", sender: self)
+                } catch let error {
+                    self.showErrorIfNeeded(error)
+                }
+            }))
+            alert.addAction(UIAlertAction(title: "キャンセル", style: .cancel))
+            self.present(alert, animated: true, completion: nil)
         default:
             break
         }
