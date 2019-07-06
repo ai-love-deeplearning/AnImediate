e50a0936ca07fda5e6fb321df7ceaedda9fb6880
k-shuya <kwamua@me.com>
2019-07-06 23:46:20 +0900

    ホームヘッダーのレイアウト追加とカメラロールで選択できるよう実装

--- AnImediate/AnImediate/HomeHeaderVC.swift
+++ AnImediate/AnImediate/HomeHeaderVC.swift
@@ -13,12 +13,20 @@ class HomeHeaderVC: UIViewController {
 
     @IBOutlet weak var visualEffectView: UIVisualEffectView!
     
+    @IBOutlet weak var iconView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         parallaxHeader?.delegate = self
         parallaxHeader?.height = 400
         parallaxHeader?.mode = .fill
+        
+        iconView.layer.cornerRadius = iconView.frame.width * 0.5
+        iconView.layer.shadowOffset = .zero
+        iconView.layer.shadowColor = UIColor.black.cgColor
+        iconView.layer.shadowOpacity = 0.6
+        iconView.layer.shadowRadius = 4
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -27,22 +35,53 @@ class HomeHeaderVC: UIViewController {
         parallaxHeader?.minimumHeight = 88
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func editBtnTapped(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            self.present(picker, animated: true, completion: nil)
+        }
+        
+        let image = UIImage(named: "pic")!
+        let imageCropVC = RSKImageCropViewController(image: image, cropMode: .circle)
+        imageCropVC.moveAndScaleLabel.text = "切り取り範囲を選択"
+        imageCropVC.cancelButton.setTitle("キャンセル", for: .normal)
+        imageCropVC.chooseButton.setTitle("完了", for: .normal)
+        imageCropVC.delegate = self
+        present(imageCropVC, animated: true)
     }
-    */
+    
+    
 
 }
 
 extension HomeHeaderVC: MXParallaxHeaderDelegate {
     func parallaxHeaderDidScroll(_ parallaxHeader: MXParallaxHeader) {
-        let alpha = 1 - min(1, parallaxHeader.progress)
+        //let alpha = 1 - min(1, parallaxHeader.progress)
+        let alpha = 1 - (parallaxHeader.progress - 0.27)
         visualEffectView.alpha = alpha
+    }
+}
+
+extension HomeHeaderVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as! UIImage
+        
+        iconView.image = image
+        self.dismiss(animated: false)
+    }
+}
+
+extension HomeHeaderVC: RSKImageCropViewControllerDelegate {
+    //キャンセルを押した時の処理
+    func imageCropViewControllerDidCancelCrop(_ controller: RSKImageCropViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //完了を押した後の処理
+    func imageCropViewController(_ controller: RSKImageCropViewController, didCropImage croppedImage: UIImage, usingCropRect cropRect: CGRect, rotationAngle: CGFloat) {
+        dismiss(animated: true)
+        iconView.image = croppedImage
     }
 }
