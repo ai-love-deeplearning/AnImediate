cd581c2e2af04574f6e00dc6749657d02b35f51d
前田陸 <>
2019-07-10 10:34:43 +0900

    アニメ詳細画面レイアウト修正

--- AnImediate/AnImediate/AnimeDetailsVC.swift
+++ AnImediate/AnImediate/AnimeDetailsVC.swift
@@ -1,39 +1,28 @@
 //
-//  AnimeDetailsHeaderVC.swift
+//  AnimeDetailsVC.swift
 //  AnImediate
 //
-//  Created by 前田陸 on 2019/07/09.
+//  Created by 前田陸 on 2019/07/10.
 //  Copyright © 2019 AI_Love_DeepLearning. All rights reserved.
 //
 
 import UIKit
-import MXParallaxHeader
 
-class AnimeDetailsHeaderVC: UIViewController {
+class AnimeDetailsVC: UIViewController {
+
     @IBOutlet weak var animeImageView: UIImageView!
     
     public var imageURL = "https://annict.imgix.net/shrine/workimage/604/image/master-0bad88db8a4e54a196b89a7ab2b59664.jpg?ixlib=rails-3.0.2&auto=format&w=270&h=360&fit=fillmax&fill=blur&s=e53bcf6d3d42be3aebcf3da767f7bb25"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.navigationController!.interactivePopGestureRecognizer!.isEnabled = false
+
         self.navigationItem.title = "アニメ詳細"
         
-        parallaxHeader?.delegate = self
-        parallaxHeader?.height = 269
-        parallaxHeader?.mode = .fill
-
         setIconImageView(imageUrlString: imageURL)
         animeImageView.contentMode = .scaleAspectFill
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        parallaxHeader?.minimumHeight = 88
-    }
-    
     private func setIconImageView(imageUrlString: String) {
         guard let iconImageUrl = URL(string: imageUrlString) else {return}
         let session = URLSession(configuration: .default)
@@ -49,7 +38,4 @@ class AnimeDetailsHeaderVC: UIViewController {
         }
         downloadImageTask.resume()
     }
-}
-
-extension AnimeDetailsHeaderVC: MXParallaxHeaderDelegate {
 }
