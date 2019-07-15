276f03d04b13ce0d8ed816cbe760443034dc0609
前田陸 <>
2019-07-15 16:29:50 +0900

    リザルト画面ファイル追加

---
+++ AnImediate/AnImediate/ResultVC.swift
@@ -0,0 +1,20 @@
+//
+//  ResultVC.swift
+//  AnImediate
+//
+//  Created by 前田陸 on 2019/07/15.
+//  Copyright © 2019 AI_Love_DeepLearning. All rights reserved.
+//
+
+import UIKit
+
+class ResultVC: UIViewController {
+
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var userCV: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
