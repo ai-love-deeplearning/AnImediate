6297bea5aa742a78869f062fd17a3380b82941c0
k-shuya <kwamua@me.com>
2019-07-15 16:11:24 +0900

    serch画面のアコーディオンテーブルの実装完了

---
+++ AnImediate/AnImediate/AccordionViewModel.swift
@@ -0,0 +1,68 @@
+//
+//  AccordionViewModel.swift
+//  AnImediate
+//
+//  Created by 川村周也 on 2019/07/15.
+//  Copyright © 2019 AI_Love_DeepLearning. All rights reserved.
+//
+
+import UIKit
+
+class AccordionViewModel {
+
+    var currentAnimation = UITableView.RowAnimation.fade
+    
+    private var sectionContents: [SectionContents] = []
+    
+    func addSectionContent(content: SectionContents) {
+        sectionContents.append(content)
+    }
+    
+    func sectionCount() -> Int {
+        return sectionContents.count
+    }
+    
+    func rowCount(section: Int) -> Int {
+        if sectionCount() == 0 { return 0 }
+        // 開いているセクションだったらgenre分の個数を返す
+        if sectionContents[section].isOpen { return sectionContents[section].genreTitles?.count ?? 0 }
+        // 開いてないセクションだったら0
+        return 0
+    }
+    
+    func categoryTitle(section: Int) -> String? {
+        if sectionCount() == 0 { return nil }
+        return sectionContents[section].categoryTitle
+    }
+    
+    func cellTitle(indexPath: IndexPath) -> String? {
+        if sectionCount() == 0 { return nil }
+        if let genreTitles = sectionContents[indexPath.section].genreTitles {
+            return genreTitles[indexPath.row]
+        }
+        return nil
+    }
+    
+    func isOpen(in section: Int) -> Bool {
+        if sectionContents.isEmpty { return false }
+        return sectionContents[section].isOpen
+    }
+    
+    func changeIsOpen(section: Int) {
+        if sectionContents.isEmpty { return }
+        let isOpen = sectionContents[section].isOpen
+        sectionContents[section].isOpen = !isOpen
+    }
+}
+
+struct SectionContents {
+    var categoryTitle: String?
+    var isOpen: Bool = false
+    var genreTitles: [String]?
+    
+    init(categoryTitle: String? = nil, isOpen: Bool = false, genreTitles: [String]? = nil) {
+        self.categoryTitle = categoryTitle
+        self.isOpen = isOpen
+        self.genreTitles = genreTitles
+    }
+}
---
+++ AnImediate/AnImediate/BroadcastTableVC.swift
@@ -0,0 +1,113 @@
+//
+//  BroadcastTableVC.swift
+//  AnImediate
+//
+//  Created by 川村周也 on 2019/07/14.
+//  Copyright © 2019 AI_Love_DeepLearning. All rights reserved.
+//
+
+import UIKit
+
+class BroadcastTableVC: UIViewController {
+    
+    private var viewModel = AccordionViewModel()
+    @IBOutlet weak var broadcastTable: UITableView!
+    
+    private let seasons: [String] = ["冬（1月 〜 3月）", "春（4月 〜 6月）", "夏（7月 〜 9月）", "秋（10月 〜 12月）"]
+    private let old: Int = 1972
+    private let latest: Int = 2019
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        broadcastTable.delegate = self
+        broadcastTable.dataSource = self
+        broadcastTable.estimatedSectionHeaderHeight = 0
+        
+        title = viewModel.currentAnimation.rowName()
+        broadcastTable.tableFooterView = UIView(frame: .zero)
+        
+        addSectionContents()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func addSectionContents() {
+        for i in (old...latest).reversed() {
+            viewModel.addSectionContent(content: SectionContents(categoryTitle: String(i) + "年", genreTitles: seasons))
+        }
+    }
+    
+    @objc func toggleCategoryHeader(gestureRecognizer: UITapGestureRecognizer) {
+        guard let header = gestureRecognizer.view as? AccordionSectionHeaderView else { return }
+        // nilにしないと上下矢印が一瞬重なって見えてしまう
+        header.setImage(isOpen: nil)
+        viewModel.changeIsOpen(section: header.section)
+        broadcastTable.beginUpdates()
+        broadcastTable.reloadSections([header.section], with: viewModel.currentAnimation)
+        broadcastTable.endUpdates()
+    }
+
+}
+
+extension BroadcastTableVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.sectionCount()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.rowCount(section: section)
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = AccordionSectionHeaderView.instance()
+        header.setTitle(title: viewModel.categoryTitle(section: section))
+        header.section = section
+        header.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(BroadcastTableVC.toggleCategoryHeader(gestureRecognizer: ))))
+        header.setImage(isOpen: viewModel.isOpen(in: section))
+        header.backgroundColor = .white
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    private func cellTitleForRowAtIndexPath(_ indexPath: IndexPath) -> String? {
+        return viewModel.cellTitle(indexPath: indexPath)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = cellTitleForRowAtIndexPath(indexPath)
+        return cell
+    }
+    
+}
+
+extension UITableView.RowAnimation {
+    func rowName() -> String {
+        switch self {
+        case .fade:
+            return "fade"
+        case .right:
+            return "right"
+        case .left:
+            return "left"
+        case .top:
+            return "top"
+        case .bottom:
+            return "bottom"
+        case .none:
+            return "none"
+        case .middle:
+            return "middle"
+        case .automatic:
+            return "automatic"
+        @unknown default:
+            return "fade"
+        }
+    }
+}
