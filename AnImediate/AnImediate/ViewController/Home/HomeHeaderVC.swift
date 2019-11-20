7ecb647f75c7fc951523fda513c3b4b4b79e3ff6
k-shuya <kwamua@me.com>
2019-11-20 16:25:06 +0900

    HomeHeaderに登録数を反映

--- AnImediate/AnImediate/ViewController/Home/HomeHeaderVC.swift
+++ AnImediate/AnImediate/ViewController/Home/HomeHeaderVC.swift
@@ -19,27 +19,25 @@ class HomeHeaderVC: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var idLabel: UILabel!
     @IBOutlet weak var commentLabel: UILabel!
-    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    @IBOutlet weak var keepCountLabel: UILabel!
+    @IBOutlet weak var nowCountLabel: UILabel!
+    @IBOutlet weak var doneCountLabel: UILabel!
+    @IBOutlet weak var stopCountLabel: UILabel!
     
     private let store = RxStore(store: AppStore.instance.homeStore)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(Realm.Configuration.defaultConfiguration.fileURL!)
-        parallaxHeader?.delegate = self
-        parallaxHeader?.height = ScreenConfig.homeParallaxHeaderHeight
-        parallaxHeader?.minimumHeight = ScreenConfig.statusBarSize.height + ScreenConfig.navigationBarHeight
-        parallaxHeader?.mode = .fill
         
-        iconView.layer.cornerRadius = iconView.frame.width * 0.5
-        iconView.layer.shadowOffset = .zero
-        iconView.layer.shadowColor = UIColor.black.cgColor
-        iconView.layer.shadowOpacity = 0.6
-        iconView.layer.shadowRadius = 4
+        initParallaxHeader()
+        initIconImageView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        initArchiveCounts()
         
         if CommonStateModel.read().isRegistered {
             setViews()
@@ -47,6 +45,34 @@ class HomeHeaderVC: UIViewController {
             // TODO:- CommonStateModel.isRegisteredを参照してfalseだったら初回登録
             //self.performSegue(withIdentifier: "toEdit", sender: nil)
         }
+    }
+    
+    private func initParallaxHeader() {
+        parallaxHeader?.delegate = self
+        parallaxHeader?.height = ScreenConfig.homeParallaxHeaderHeight
+        parallaxHeader?.minimumHeight = ScreenConfig.statusBarSize.height + ScreenConfig.navigationBarHeight
+        parallaxHeader?.mode = .fill
+    }
+    
+    private func initIconImageView() {
+        iconView.layer.cornerRadius = iconView.frame.width * 0.5
+        iconView.layer.shadowOffset = .zero
+        iconView.layer.shadowColor = UIColor.black.cgColor
+        iconView.layer.shadowOpacity = 0.6
+        iconView.layer.shadowRadius = 4
+    }
+    
+    private func initArchiveCounts() {
+        let myID = AccountModel.read().userID
+        let keepModel = ArchiveModel.read(uid: myID).filter("animeStatus == %@", AnimeStatusType.keep.rawValue)
+        let nowModel = ArchiveModel.read(uid: myID).filter("animeStatus == %@", AnimeStatusType.now.rawValue)
+        let doneModel = ArchiveModel.read(uid: myID).filter("animeStatus == %@", AnimeStatusType.done.rawValue)
+        let stopModel = ArchiveModel.read(uid: myID).filter("animeStatus == %@", AnimeStatusType.stop.rawValue)
+        
+        keepCountLabel.text = String(keepModel.count)
+        nowCountLabel.text = String(nowModel.count)
+        doneCountLabel.text = String(doneModel.count)
+        stopCountLabel.text = String(stopModel.count)
     }
     
     private func setViews() {
