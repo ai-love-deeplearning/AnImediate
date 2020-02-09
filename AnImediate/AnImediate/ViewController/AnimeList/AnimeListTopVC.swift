455934291d37a47bc2bdf87ec94eb8fb24301f56
前田陸 <>
2020-02-10 00:06:36 +0900

    ジャンルCellの画面遷移を実装

--- AnImediate/AnImediate/ViewController/AnimeList/AnimeListTopVC.swift
+++ AnImediate/AnImediate/ViewController/AnimeList/AnimeListTopVC.swift
@@ -32,6 +32,18 @@ class AnimeListTopVC: UIViewController {
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var currentTermBtn: UIButton!
     @IBOutlet weak var rankingBtn: UIButton!
+    @IBOutlet weak var sfBtn: UIButton!
+    @IBOutlet weak var battleBtn: UIButton!
+    @IBOutlet weak var horrorBtn: UIButton!
+    @IBOutlet weak var robotBtn: UIButton!
+    @IBOutlet weak var loveBtn: UIButton!
+    @IBOutlet weak var comedyBtn: UIButton!
+    @IBOutlet weak var dailyBtn: UIButton!
+    @IBOutlet weak var sportsBtn: UIButton!
+    @IBOutlet weak var dramaBtn: UIButton!
+    @IBOutlet weak var histBtn: UIButton!
+    @IBOutlet weak var warBtn: UIButton!
+    @IBOutlet weak var otherBtn: UIButton!
     
     weak var delegate: AnimeListTopVCDelegate?
     
@@ -215,6 +227,102 @@ class AnimeListTopVC: UIViewController {
                     self.store.dispatch(AnimeListTableViewAction.Initialize(contentType: .ranking))
             })
             .disposed(by: disposeBag)
+        
+        sfBtn.rx.tap.asDriver()
+            .coolTime()
+            .drive(
+                onNext: { [unowned self] in
+                    self.store.dispatch(AnimeListTableViewAction.Initialize(contentType: .sfGenre))
+            })
+            .disposed(by: disposeBag)
+        
+        battleBtn.rx.tap.asDriver()
+            .coolTime()
+            .drive(
+                onNext: { [unowned self] in
+                    self.store.dispatch(AnimeListTableViewAction.Initialize(contentType: .battleGenre))
+            })
+            .disposed(by: disposeBag)
+        
+        horrorBtn.rx.tap.asDriver()
+            .coolTime()
+            .drive(
+                onNext: { [unowned self] in
+                    self.store.dispatch(AnimeListTableViewAction.Initialize(contentType: .horrorGenre))
+            })
+            .disposed(by: disposeBag)
+        
+        robotBtn.rx.tap.asDriver()
+            .coolTime()
+            .drive(
+                onNext: { [unowned self] in
+                    self.store.dispatch(AnimeListTableViewAction.Initialize(contentType: .robotGenre))
+            })
+            .disposed(by: disposeBag)
+        
+        loveBtn.rx.tap.asDriver()
+            .coolTime()
+            .drive(
+                onNext: { [unowned self] in
+                    self.store.dispatch(AnimeListTableViewAction.Initialize(contentType: .loveGenre))
+            })
+            .disposed(by: disposeBag)
+        
+        comedyBtn.rx.tap.asDriver()
+            .coolTime()
+            .drive(
+                onNext: { [unowned self] in
+                    self.store.dispatch(AnimeListTableViewAction.Initialize(contentType: .comedyGenre))
+            })
+            .disposed(by: disposeBag)
+        
+        dailyBtn.rx.tap.asDriver()
+            .coolTime()
+            .drive(
+                onNext: { [unowned self] in
+                    self.store.dispatch(AnimeListTableViewAction.Initialize(contentType: .dailyGenre))
+            })
+            .disposed(by: disposeBag)
+        
+        sportsBtn.rx.tap.asDriver()
+            .coolTime()
+            .drive(
+                onNext: { [unowned self] in
+                    self.store.dispatch(AnimeListTableViewAction.Initialize(contentType: .sportsGenre))
+            })
+            .disposed(by: disposeBag)
+        
+        dramaBtn.rx.tap.asDriver()
+            .coolTime()
+            .drive(
+                onNext: { [unowned self] in
+                    self.store.dispatch(AnimeListTableViewAction.Initialize(contentType: .dramaGenre))
+            })
+            .disposed(by: disposeBag)
+        
+        histBtn.rx.tap.asDriver()
+            .coolTime()
+            .drive(
+                onNext: { [unowned self] in
+                    self.store.dispatch(AnimeListTableViewAction.Initialize(contentType: .histGenre))
+            })
+            .disposed(by: disposeBag)
+        
+        warBtn.rx.tap.asDriver()
+            .coolTime()
+            .drive(
+                onNext: { [unowned self] in
+                    self.store.dispatch(AnimeListTableViewAction.Initialize(contentType: .warGenre))
+            })
+            .disposed(by: disposeBag)
+        
+        otherBtn.rx.tap.asDriver()
+            .coolTime()
+            .drive(
+                onNext: { [unowned self] in
+                    self.store.dispatch(AnimeListTableViewAction.Initialize(contentType: .otherGenre))
+            })
+            .disposed(by: disposeBag)
     }
     
     private func bindState() {
@@ -265,11 +373,59 @@ class AnimeListTopVC: UIViewController {
             break
         case "fromThisTerm":
             let nextVC = segue.destination as! AnimeListTableVC
-            nextVC.navigationItem.title = "今期アニメ"
+            nextVC.navigationItem.title = AnimeTableContentType.currentTerm.rawValue + "アニメ"
             break
         case "fromRank":
             let nextVC = segue.destination as! AnimeListTableVC
-            nextVC.navigationItem.title = "ランキング"
+            nextVC.navigationItem.title = AnimeTableContentType.ranking.rawValue
+            break
+        case "fromSF":
+            let nextVC = segue.destination as! AnimeListTableVC
+            nextVC.navigationItem.title = AnimeTableContentType.sfGenre.rawValue
+            break
+        case "fromBattle":
+            let nextVC = segue.destination as! AnimeListTableVC
+            nextVC.navigationItem.title = AnimeTableContentType.battleGenre.rawValue
+            break
+        case "fromHorror":
+            let nextVC = segue.destination as! AnimeListTableVC
+            nextVC.navigationItem.title = AnimeTableContentType.horrorGenre.rawValue
+            break
+        case "fromRobot":
+            let nextVC = segue.destination as! AnimeListTableVC
+            nextVC.navigationItem.title = AnimeTableContentType.robotGenre.rawValue
+            break
+        case "fromLove":
+            let nextVC = segue.destination as! AnimeListTableVC
+            nextVC.navigationItem.title = AnimeTableContentType.loveGenre.rawValue
+            break
+        case "fromComedy":
+            let nextVC = segue.destination as! AnimeListTableVC
+            nextVC.navigationItem.title = AnimeTableContentType.comedyGenre.rawValue
+            break
+        case "fromDaily":
+            let nextVC = segue.destination as! AnimeListTableVC
+            nextVC.navigationItem.title = AnimeTableContentType.dailyGenre.rawValue
+            break
+        case "fromSports":
+            let nextVC = segue.destination as! AnimeListTableVC
+            nextVC.navigationItem.title = AnimeTableContentType.sportsGenre.rawValue
+            break
+        case "fromDrama":
+            let nextVC = segue.destination as! AnimeListTableVC
+            nextVC.navigationItem.title = AnimeTableContentType.dramaGenre.rawValue
+            break
+        case "fromHist":
+            let nextVC = segue.destination as! AnimeListTableVC
+            nextVC.navigationItem.title = AnimeTableContentType.histGenre.rawValue
+            break
+        case "fromWar":
+            let nextVC = segue.destination as! AnimeListTableVC
+            nextVC.navigationItem.title = AnimeTableContentType.warGenre.rawValue
+            break
+        case "fromOther":
+            let nextVC = segue.destination as! AnimeListTableVC
+            nextVC.navigationItem.title = AnimeTableContentType.otherGenre.rawValue
             break
         default:
             break
@@ -413,6 +569,54 @@ private extension RxStore where AnyStateType == AnimeListViewState {
     
     var ranking: Driver<[AnimeModel]> {
         return state.mapDistinct { $0.ranking }.skipNil()
+    }
+    
+    var sfGenre: Driver<[AnimeModel]> {
+        return state.mapDistinct { $0.sfGenre }.skipNil()
+    }
+    
+    var battleGenre: Driver<[AnimeModel]> {
+        return state.mapDistinct { $0.battleGenre }.skipNil()
+    }
+    
+    var horrorGenre: Driver<[AnimeModel]> {
+        return state.mapDistinct { $0.horrorGenre }.skipNil()
+    }
+    
+    var robotGenre: Driver<[AnimeModel]> {
+        return state.mapDistinct { $0.robotGenre }.skipNil()
+    }
+    
+    var loveGenre: Driver<[AnimeModel]> {
+        return state.mapDistinct { $0.loveGenre }.skipNil()
+    }
+    
+    var comedyGenre: Driver<[AnimeModel]> {
+        return state.mapDistinct { $0.comedyGenre }.skipNil()
+    }
+    
+    var dailyGenre: Driver<[AnimeModel]> {
+        return state.mapDistinct { $0.dailyGenre }.skipNil()
+    }
+    
+    var sportsGenre: Driver<[AnimeModel]> {
+        return state.mapDistinct { $0.sportsGenre }.skipNil()
+    }
+    
+    var dramaGenre: Driver<[AnimeModel]> {
+        return state.mapDistinct { $0.dramaGenre }.skipNil()
+    }
+    
+    var histGenre: Driver<[AnimeModel]> {
+        return state.mapDistinct { $0.histGenre }.skipNil()
+    }
+    
+    var warGenre: Driver<[AnimeModel]> {
+        return state.mapDistinct { $0.warGenre }.skipNil()
+    }
+    
+    var otherGenre: Driver<[AnimeModel]> {
+        return state.mapDistinct { $0.otherGenre }.skipNil()
     }
     
     var error: Driver<AnimediateError> {
